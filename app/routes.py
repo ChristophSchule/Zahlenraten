58ef96a5e62cf6a27ea55ec52d12292fcdf8637d
Marvin <marvin.zakrzewski@students.tbs1.de>
2025-10-30 11:49:33 +0100

    game over

--- app/routes.py
+++ app/routes.py
@@ -6,19 +6,30 @@ bp = Blueprint('main', __name__)
 
 @bp.route('/', methods=["GET", "POST"])
 def index():
-    guesses = session.get('guesses', [])
-    return render_template('index.html', guesses=guesses)
+    return start_game()
+
 
 
 @bp.route("/guess", methods=["POST"])
 def guess():
     action = request.form.get("action")
     if action == "restart":
-        session['guesses'] = []
-        return render_template("index.html", guesses=[])
+        return start_game()
     guess = request.form.get("guess")
-    handle_guess(guess)
+    result = handle_guess(guess)
     guesses = session.get('guesses', [])
     guesses.append(guess)
     session['guesses'] = guesses
-    return render_template("index.html", guesses=guesses)
+    number_to_guess = session.get('number_to_guess')
+    if result == "correct":
+        return render_template("game_over.html", guesses=guesses, number_to_guess=number_to_guess)
+    else:
+        return render_template("index.html", guesses=guesses, number_to_guess=number_to_guess)
+
+
+def start_game():
+    session['guesses'] = []
+    # Generate a random number between 0 and 100 and store in session
+    session['number_to_guess'] = generate_random_number(0, 100)
+    session['number_of_attempts'] = 0
+    return render_template("index.html", guesses=[], number_to_guess=session['number_to_guess'])
